3045f7f94bcdbfa73fc827b7b0be8b0de87d0b9e
Henrique Cesar Carvalho de Resende <henrique.carvalhoderesende@uantwerpen.be>
2021-07-02 15:23:58 +0200

    last commit

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 # This script was developed to run runtime and ryu simultaneously
 
+# Add the openvpn connection here
+
+
+# Starting services
 python3 ./empower-ryu/bin/ryu-manager --observe-links ryu.app.intent &
 
 ./empower-runtime/empower-runtime.py &
